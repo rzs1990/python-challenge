db964ff288da7e62eb27dbcdbd1501e81b430278
rzs1990 <88170749+rzs1990@users.noreply.github.com>
2021-08-28 21:16:36 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+import csv
+
+# Can't figure out my path but I'm gonna try and write the code (won't know if it works)
+
+# Set Path
+csvpath = os.path.join('"..", "Resources", "budget_data.csv"')
+
+#Variables
+total_votes = 0
+cand_vote = 0
+perc_votes = 0
+winner = []
+
+
+
